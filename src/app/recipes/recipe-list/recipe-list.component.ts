5cfa2fa8ada69085f97d9433b3d094d630ea8747
vrnico <nico.daunt@gmail.com>
2018-06-09 16:40:02 -0700

    add Recipe details to view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'http://www.frugalupstate.com/wp-content/uploads/2015/07/NY-hotdog-4-1024x768.jpg')
+
+  ];
+
 
   constructor() { }
 
