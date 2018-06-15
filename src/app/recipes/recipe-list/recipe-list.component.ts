635a3567246be93196e89a973ea0e1a60a84b57f
vrnico <nico.daunt@gmail.com>
2018-06-15 11:26:21 -0700

    added Recipe Detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'http://www.frugalupstate.com/wp-content/uploads/2015/07/NY-hotdog-4-1024x768.jpg')
+    new Recipe('Hot Dogs', 'This is a test', 'http://www.frugalupstate.com/wp-content/uploads/2015/07/NY-hotdog-4-1024x768.jpg'),
+    new Recipe('Macaroni', 'This is a test', 'https://uz71pyzpz0-flywheel.netdna-ssl.com/wp-content/uploads/2017/08/garlic-parmesan-macaroni-cheese-3-700x726.jpg')
 
   ];
 
@@ -16,6 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
